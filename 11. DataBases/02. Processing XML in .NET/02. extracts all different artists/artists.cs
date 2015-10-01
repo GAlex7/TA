753aa4aee0c3e52b5c9b7cf627ac9dbd65d7f4e3
GA <GA@abv.bg>
2015-10-01 18:37:45 +0300

    xxx

--- 11. DataBases/02. Processing XML in .NET/02. extracts all different artists/artists.cs
+++ 11. DataBases/02. Processing XML in .NET/02. extracts all different artists/artists.cs
@@ -2,20 +2,42 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Xml;
 
-    class DOMParser
+    public class DOMParser
     {
         public static void Main()
         {
             XmlDocument doc = new XmlDocument();
             doc.Load("../../../01. XML file representing catalogue/catalog.xml");
-            Console.WriteLine("Loaded XML document:");
-            Console.WriteLine(doc.OuterXml);
-            Console.WriteLine();
+            var root = doc.DocumentElement;
+
+            foreach (var pair in GetArtists(root))
+            {
+                Console.WriteLine("{0} - {1} album(s)", pair.Key, pair.Value);
+            }
+        }
+
+        private static Dictionary<string, int> GetArtists(XmlElement root)
+        {
+            var artistsAndAlbums = new Dictionary<string, int>();
+            var artists = root.GetElementsByTagName("artist");
+
+            foreach (XmlElement artist in artists)
+            {
+                var artistName = artist.InnerText;
+
+                if (artistsAndAlbums.ContainsKey(artistName))
+                {
+                    artistsAndAlbums[artistName] += 1;
+                }
+                else
+                {
+                    artistsAndAlbums.Add(artistName, 1);
+                }
+            }
+
+            return artistsAndAlbums;
         }
     }
 }
