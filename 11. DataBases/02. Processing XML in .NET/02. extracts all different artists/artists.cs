0300d818db83074ca3ead6453cb110164c8fdd32
GA <GA@abv.bg>
2015-10-01 18:25:57 +0300

    work...

--- 11. DataBases/02. Processing XML in .NET/02. extracts all different artists/artists.cs
+++ 11. DataBases/02. Processing XML in .NET/02. extracts all different artists/artists.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _02.extracts_all_different_artists
+﻿namespace _02.extracts_all_different_artists
 {
-    class artists
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    class DOMParser
     {
+        public static void Main()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../../01. XML file representing catalogue/catalog.xml");
+            Console.WriteLine("Loaded XML document:");
+            Console.WriteLine(doc.OuterXml);
+            Console.WriteLine();
+        }
     }
 }
