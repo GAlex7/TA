df92b972abcf424d71541190eaf314c64851f38f
GA <GA@abv.bg>
2015-10-01 18:01:17 +0300

    dataBases first HW

---
+++ 11. DataBases/01.XMLBasics/XML Basics/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        XmlDocument doc = new XmlDocument();
+        doc.Load("../../students.xml");
+        Console.WriteLine("Loaded XML document:");
+        Console.WriteLine(doc.OuterXml);
+        Console.WriteLine();
+
+        XmlNode rootNode = doc.DocumentElement;
+        Console.WriteLine("Root node: {0}", rootNode.Name);
+
+    }
+}
---
+++ 11. DataBases/01.XMLBasics/XML Basics/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../../students.xml");
+            Console.WriteLine("Loaded XML document:");
+            Console.WriteLine(doc.OuterXml);
+            Console.WriteLine();
+
+            XmlNode rootNode = doc.DocumentElement;
+            Console.WriteLine("Root node: {0}", rootNode.Name);
+
+        }
+    }
+}
