b74d87a42e2275fbddaa329d471e6de53bd5d499
GAlex <GAlexGr@gmail.com>
2015-10-29 16:34:53 +0200

    Mobile track!!!

---
+++ 12. DSA/02. Linear-Data-Structures/LinearDS/1.SumAndAverage/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace SumAndAverage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter a number on separated lines, for end enter blank line.");
+            var input = string.Empty;
+            var sequence = new List<int>();
+
+            while ((input = Console.ReadLine()) != string.Empty)
+            {
+                sequence.Add(int.Parse(input));
+            }
+
+            Console.WriteLine("sum of the elements of the sequence is {0} and average is {1}.", sequence.Sum(), sequence.Average());
+        }
+    }
+}
---
+++ 12. DSA/02. Linear-Data-Structures/LinearDS/2.ReverseNumbers/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace ReverseNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                   .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                   .Select(n => int.Parse(n))
+                   .ToList();
+
+            var reversedNumbers = ReverseNumbers(numbers);
+
+            foreach (var number in reversedNumbers)
+            {
+                Console.Write(number);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static ICollection<int> ReverseNumbers(ICollection<int> numbers)
+        {
+            var stackForReversing = new Stack<int>();
+
+            numbers
+                .ToList()
+                .ForEach(n => stackForReversing.Push(n));
+
+            var reversedNumbers = new List<int>();
+
+            while (stackForReversing.Count > 0)
+            {
+                reversedNumbers.Add(stackForReversing.Pop());
+            }
+
+            return reversedNumbers;
+        }
+    }
+}
