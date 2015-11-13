fb190c5aad8884b952bcc1e7090ede62c60af781
GAlex <GAlexGr@gmail.com>
2015-11-13 11:54:34 +0200

    8

---
+++ 12. DSA/08. Combinatorics/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Rabbits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = @"9
+2
+2
+44
+2
+2
+2
+444
+2
+2";
+            Console.SetIn(new StringReader(input));
+
+            var n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var current = int.Parse(Console.ReadLine());
+                if (!dict.ContainsKey(current))
+                {
+                    dict.Add(current, 0);
+                }
+
+                dict[current] += 1;
+            }
+
+            var result = 0;
+            foreach (var item in dict)
+            {
+                var x = item.Value < 3 ? 1 : 1 + item.Value / (item.Key + 1);
+                //Console.WriteLine("{0} x = {1}",item.Key, x);
+                result += x * (item.Key + 1);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 12. DSA/08. Combinatorics/ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApplication2
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var firstVar = line[1];
+            var secondVar = line[3];
+
+            line = Console.ReadLine();
+            line = Console.ReadLine(); // I don't know why I have to do this?! but bgCoder....
+            var power = int.Parse(line);
+
+            ulong x = 1;
+            ulong row = (ulong)power;
+            var binominalCoefficients = new ulong[row + 1];
+            for (ulong col = 0; col <= row; col++)
+            {
+                binominalCoefficients[col] = x;
+                x = x * (row - col) / (col + 1);
+            }
+
+            var result = new StringBuilder();
+
+            result.AppendFormat("({0}^{1})", firstVar, power);
+            for (int col = 1; col <= power; col++)
+            {
+                if (power - col == 0)
+                {
+                    result.AppendFormat("+({0}^{1})", secondVar, col);
+                }
+                else
+                {
+                    result.AppendFormat("+{0}({1}^{2})({3}^{4})", binominalCoefficients[col], firstVar, power - col, secondVar, col);
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
