1995653d09f6f7f3439dff6f13764ce5b091a38f
GAlex <GAlexGr@gmail.com>
2015-11-13 22:19:07 +0200

    HW 08

---
+++ 12. DSA/08. Combinatorics/ConsoleApplication3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BinaryPasswords
+{
+    using System;
+    using System.Linq;
+
+    public class StarUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            int pow = input.Where(a => a == '*').Count();
+
+            long baseN = 2;
+            long result = 1;
+
+            for (int i = 0; i < pow; i++)
+            {
+                result *= baseN;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
